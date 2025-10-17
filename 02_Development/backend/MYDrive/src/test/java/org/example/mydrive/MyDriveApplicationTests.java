bf567c5368646ae767edb6d9d7dcfc5065c3bcfa
SOvnarski <SOvnarski@technologica.com>
2025-10-17 16:46:31 +0300

    Initial commit

---
+++ 02_Development/backend/MYDrive/src/test/java/org/example/mydrive/MyDriveApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.mydrive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyDriveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
